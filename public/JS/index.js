f444865f617e8140755dc82632da057cd664523e
Jorge Lucas <jorgelucasxx27@gmail.com>
2025-11-25 13:12:25 -0300

    Primeiro comite do projeto Codai-GrowDev

---
+++ public/JS/index.js
@@ -0,0 +1,120 @@
+const myModal= new bootstrap.Modal("#register-modal");
+let logged= sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+checkLogged();
+
+//LOGAR NO SISTEMA
+document.getElementById("login-form").addEventListener("submit", function(logar){
+    logar.preventDefault();
+
+    const email= document.getElementById("email-input").value;
+    const password= document.getElementById("password-input").value;
+    const checkSession= document.getElementById("session-check").checked;
+
+    const account= getAccount(email);
+
+    if(!account){
+        alert("Verifique o usuário ou a senha");
+        return;
+
+    }
+    if(account){
+        if(account.password !==password){
+            alert("Verifique o usuário ou a senha");
+            return;
+        }
+
+
+        saveSession(email, checkSession);
+    
+
+    
+
+    window.location.href="home.html";
+}
+
+
+
+});
+
+
+
+// CRIAR CONTA
+document.getElementById("creat-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    
+    const email= document.getElementById("email-create-input").value;
+    const password= document.getElementById("password-create-input").value;
+
+    if(email.length < 5){
+        alert("Preencha o email de forma válida. ");
+        return;
+
+    }
+
+    if(password.length <4 || /\s/.test(password)){
+        alert("Preencha uma senha válida, com no mínimo 4 digitos");
+        return;
+
+    }
+
+    saveAccount({
+        login: email,
+        password: password,
+        transactions: []
+    });
+
+     
+    myModal.hide();
+
+    alert("Conta criada com sucesso!");
+
+});
+
+
+// Salvar conta
+function saveAccount(data){
+    localStorage.setItem(data.login, JSON.stringify(data));
+
+}
+
+function getAccount(key){
+    const account= localStorage.getItem(key);
+    if(account){
+        return JSON.parse(account);
+
+    }
+
+    else{
+        return "";
+    }
+
+}
+
+// Checar sessão
+function checkLogged() {
+    if(session){
+        sessionStorage.setItem("logged", session);
+        logged= session;
+    }
+    if(logged){
+        saveSession(logged, session);
+
+        window.location.href= "home.html";
+
+    }
+    
+
+}
+
+
+function saveSession(data, saveSession){
+    if(saveSession){
+        localStorage.setItem("session", data);
+    }
+    else{
+        sessionStorage.setItem("logged", data);
+    }
+
+}
---
+++ public/JS/transactions.js
@@ -0,0 +1,104 @@
+const myModal = new bootstrap.Modal("#transactions-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+let data = {
+    transactions: []
+};
+
+document.getElementById("button-logout").addEventListener("click", logout);
+
+
+
+//Enviar lançamentos
+document.getElementById("transactions-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const value = parseFloat(document.getElementById("value-input").value);
+    const description = document.getElementById("description-input").value;
+    const date = document.getElementById("date-input").value;
+    const type = document.querySelector('input[name="type-input"]:checked').value;
+
+    data.transactions.unshift({
+        value: value, type: type, description: description, date: date
+    });
+
+    saveDate(data);
+    e.target.reset();
+    myModal.hide();
+
+    getTramsactions();
+
+    alert("Lançamento adicionado com sucesso!")
+});
+
+
+
+
+checkLogged();
+
+function checkLogged() {
+    if (session) {
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+    if (!logged) {
+        window.location.href = "index.html";
+        return;
+
+    }
+
+    const dataUser = localStorage.getItem(logged)
+    if (dataUser) {
+        data = JSON.parse(dataUser);
+
+    }
+
+
+    getTramsactions();
+
+
+}
+
+
+// Função de sair da conta
+function logout() {
+    sessionStorage.removeItem("logged");
+    localStorage.removeItem("session");
+
+    window.location.href = "index.html";
+}
+
+
+function getTramsactions() {
+    const transactions = data.transactions;
+    let transactionsHtml = ``;
+
+    if (transactions.length) {
+        transactions.forEach((item) => {
+            let type = "Entrada";
+
+            if (item.type === 2) {
+                type = "Saída";
+
+            }
+
+            transactionsHtml += ` 
+                <tr>
+                                                <th scope="row">${item.date}</th>
+                                                <td>${item.value.toFixed(2)}</td>
+                                                <td>${type}</td>
+                                                <td>${item.description}</td>
+                                            </tr>
+            
+            `
+
+        });
+    }
+
+    document.getElementById("transactions-list").innerHTML = transactionsHtml;
+}
+
+function saveDate(data) {
+    localStorage.setItem(data.login, JSON.stringify(data))
+
+}
